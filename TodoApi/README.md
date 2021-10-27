e241ed9d09b3510053988f4d3b1d5b4dd4b1861c
Albert <albertvijai@gmail.com>
2021-10-26 21:30:48 -0500

    Initial Spring-Boot API template

---
+++ TodoApi/README.md
@@ -0,0 +1,4 @@
+# TodoApi 
+
+Get Started: 
+https://docs.microsoft.com/en-us/aspnet/core/tutorials/first-web-api?view=aspnetcore-5.0&tabs=visual-studio 
---
+++ spring-boot-template/src/test/java/com/api/springboottemplate/SpringBootTemplateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.springboottemplate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootTemplateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
