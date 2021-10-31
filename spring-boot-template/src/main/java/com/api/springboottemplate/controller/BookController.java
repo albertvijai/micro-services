a7337cbee9247fa8c38ce31244d77db9c892dc8a
Albert <albertvijai@gmail.com>
2021-10-30 22:50:19 -0500

    Fixed swaggercodegen support

--- spring-boot-template/src/main/java/com/api/springboottemplate/controller/BookController.java
+++ spring-boot-template/src/main/java/com/api/springboottemplate/controller/BookController.java
@@ -1,8 +1,27 @@
 package com.api.springboottemplate.controller;
 
+import io.swagger.annotations.Api;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
+import com.api.swaggercodgen.api.BookApi;
+import com.api.swaggercodgen.model.Book;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
-public class BookController {
+public class BookController implements BookApi{
 
+    @Override
+    public ResponseEntity<String> addBook(Book book) {
+        return ResponseEntity.ok(book.getTitle()+" is added");
+    }
+
+    @Override
+    public ResponseEntity<List<Book>> getBooks() {
+        List<Book> bookList = new ArrayList<>();
+        bookList.add(new Book().title("Name of the Wind").author("Patrick"));
+        bookList.add(new Book().title("Mistborn").author("Brandon"));
+        return ResponseEntity.ok(bookList);
+    }
 }
